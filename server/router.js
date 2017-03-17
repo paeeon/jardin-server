d8c79f6505dc74c52fd0a873ab94111a4f7b0d09
Lily Jen <lily@lilyjen.com>
2017-03-16 22:27:58 -0400

    Revert "Merge branch 'master' of https://github.com/paeeon/jardin-server"
    This reverts commit a9abd4bbe73581221e4556ab1beea6ad737e5cac, reversing
    changes made to b64f37f8eeac885c87b87d2a075f1900cb5162aa.

--- server/router.js
+++ server/router.js
@@ -3,27 +3,22 @@ var router = express.Router();
 var fs = require('fs');
 var User = require('../db/models/User');
 var bcrypt = require('bcrypt');
-var session = require('express-session');
+var jwt = require('jsonwebtoken');
 var ms = require('ms');
 var env = require('../env');
 
-// REMINDER: Add functionality here later so that the secure: true key-value
-// pair is set when in production. 
-app.use(session({
-  secret: env.sessionsSecret,
-  httpOnly: true,
-  cookie: {
-    maxAge: 60000
-  }
-}));
-
 router.post('/users/register', function(req, res) {
   res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-  bcrypt.hash(req.body.pass, 4).then(function(encrypted) {
-    return User.create({email: req.body.email, pass: encrypted, firstName: req.body.firstName});
-  }).then(function(user) {
-    res.send(user);
-  }).catch(errorHandler);
+  bcrypt.hash(req.body.pass, 4)
+    .then(function(encrypted) {
+       return User.create({
+         email: req.body.email,
+         pass: encrypted,
+         firstName: req.body.firstName
+       });
+    }).then(function(user) {
+      res.send(user);
+    }).catch(errorHandler);
 });
 
 router.post('/users/login', function(req, res) {
@@ -34,7 +29,41 @@ router.post('/users/login', function(req, res) {
     where: {
       email: req.body.email
     }
-  }).then(function(user) {});
+  }).then(function(user) {
+    foundUser = user;
+    return bcrypt.compare(req.body.pass, user.pass);
+  }).then(function(result) {
+    console.log("The result of the bcrypt compare is", result);
+    // If the user successfully entered their password…
+    if (result) {
+      return jwt.sign(
+        // payload
+        {
+          "iss": "playjard.in",
+          "exp": ms('3 months'),
+          "firstName": foundUser.firstName,
+          "email": foundUser.email,
+          "id": foundUser.id
+        },
+        // secret
+        env.jwtSecret,
+        // algorithm
+        { algorithm: 'HS256' },
+        // callback
+        function tokenSignDone(err, token) {
+          if (err) {
+            console.error(err);
+            throw new Error('An error occurred while signing the JWT!');
+          }
+          console.log('JWT signed! here it is', token);
+          res.send(token);
+        }
+      );
+    } else {
+      // If the user entered the wrong password…
+      console.error('Wrong password entered!');
+    }
+  })
 })
 
 router.use('/', function(req, res) {
